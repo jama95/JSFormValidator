c77145e5c4052a0c00eb413856c58f7efc656f2c
jama95 <joralmiga@hotmail.com>
2024-05-14 00:01:26 -0500

    improvements

--- src/js/modifiers.ts
+++ src/js/modifiers.ts
@@ -5,58 +5,72 @@ import {
   camel_pascal,
   currencyFormat,
 } from "./utils";
-(function () {
-  configuration.modifiers["capital"] = {
-    name: "capital",
-    modifierFunction: function (value) {
-      return capitalizedWords(value);
-    },
-  };
-  configuration.modifiers["sentence"] = {
-    name: "sentence",
-    modifierFunction: function (value) {
-      return sentenceCase(value);
-    },
-  };
-  configuration.modifiers["camel"] = {
-    name: "camel",
-    modifierFunction: function (value) {
-      return camel_pascal(value, false);
-    },
-  };
-  configuration.modifiers["pascal"] = {
-    name: "pascal",
-    modifierFunction: function (value) {
-      return camel_pascal(value, true);
-    },
-  };
-  configuration.modifiers["currency"] = {
-    name: "currency",
-    modifierFunction: function (value, input) {
-      const d = input.getAttribute("data-fm-decimals");
-      const locale = input.getAttribute("data-fm-locale") || undefined,
-        currency = input.getAttribute("data-fm-currency") || undefined,
-        decimals = d ? parseInt(d) : undefined;
-      if (/[^\d.]/.test(value)) return value;
-      return currencyFormat(value, language, locale, currency, decimals);
-    },
-  };
-  configuration.modifiers["trim"] = {
-    name: "trim",
-    modifierFunction: function (value) {
-      return value.trim();
-    },
-  };
-  configuration.modifiers["trimLeft"] = {
-    name: "trimLeft",
-    modifierFunction: function (value) {
-      return value.trimStart();
-    },
-  };
-  configuration.modifiers["trimRight"] = {
-    name: "trimRight",
-    modifierFunction: function (value) {
-      return value.trimEnd();
-    },
-  };
-})();
+
+/* Converts the first letter of each word in the text to uppercase */
+configuration.modifiers["capital"] = {
+  name: "capital",
+  modifierFunction: function (value) {
+    return capitalizedWords(value);
+  },
+};
+
+/* Converts the first letter of the text to uppercase */
+configuration.modifiers["sentence"] = {
+  name: "sentence",
+  modifierFunction: function (value) {
+    return sentenceCase(value);
+  },
+};
+
+/* Converts the first letter of each word in the text to uppercase and removes all the spaces */
+configuration.modifiers["camel"] = {
+  name: "camel",
+  modifierFunction: function (value) {
+    return camel_pascal(value, false);
+  },
+};
+
+/* Converts the first letter of each word(except for the first) in the text to uppercase and removes all the spaces */
+configuration.modifiers["pascal"] = {
+  name: "pascal",
+  modifierFunction: function (value) {
+    return camel_pascal(value, true);
+  },
+};
+
+/* Convert a number to currency format */
+configuration.modifiers["currency"] = {
+  name: "currency",
+  modifierFunction: function (value, input) {
+    const d = input.getAttribute("data-fm-decimals");
+    const locale = input.getAttribute("data-fm-locale") || undefined,
+      currency = input.getAttribute("data-fm-currency") || undefined,
+      decimals = d ? parseInt(d) : undefined;
+    if (/[^\d.]/.test(value)) return value;
+    return currencyFormat(value, language, locale, currency, decimals);
+  },
+};
+
+/* Removes the leading and trailing white space and line terminator characters from a string */
+configuration.modifiers["trim"] = {
+  name: "trim",
+  modifierFunction: function (value) {
+    return value.trim();
+  },
+};
+
+/* Removes the leading white space and line terminator characters from a string */
+configuration.modifiers["trimLeft"] = {
+  name: "trimLeft",
+  modifierFunction: function (value) {
+    return value.trimStart();
+  },
+};
+
+/* Removes the trailing white space and line terminator characters from a string */
+configuration.modifiers["trimRight"] = {
+  name: "trimRight",
+  modifierFunction: function (value) {
+    return value.trimEnd();
+  },
+};
